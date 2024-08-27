4ac9789fa1147fe26a9cb2d2999cf340bbbf1e91
Rod Johnson <johnsonroda@gmail.com>
2024-08-27 09:11:44 -0700

    Initializr

---
+++ src/main/kotlin/springrod/localrag/LocalragApplication.kt
@@ -0,0 +1,11 @@
+package springrod.localrag
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LocalragApplication
+
+fun main(args: Array<String>) {
+	runApplication<LocalragApplication>(*args)
+}
---
+++ src/test/kotlin/springrod/localrag/LocalragApplicationTests.kt
@@ -0,0 +1,13 @@
+package springrod.localrag
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LocalragApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
