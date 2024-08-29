580ae01530042d8642b06b7c9fb9d5ab81f8fc32
Rod Johnson <johnsonroda@gmail.com>
2024-08-29 13:19:27 -0700

    Fix template reloading and fix test

--- src/test/kotlin/springrod/localrag/ChatServiceTest.kt
+++ src/test/kotlin/springrod/localrag/ChatServiceTest.kt
@@ -1,11 +1,15 @@
 package springrod.localrag
 
 import org.junit.jupiter.api.Test
+import org.springframework.ai.chat.memory.ChatMemory
+import org.springframework.ai.chat.memory.InMemoryChatMemory
 import org.springframework.ai.embedding.EmbeddingModel
 import org.springframework.ai.vectorstore.VectorStore
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.test.context.SpringBootTest
 
+@ConditionalOnProperty("\${OPENAI_API_KEY}")
 @SpringBootTest
 class ChatServiceTest {
 
@@ -18,8 +22,13 @@ class ChatServiceTest {
     @Autowired
     private lateinit var vectorStore: VectorStore
 
+    private val chatMemory: ChatMemory = InMemoryChatMemory()
+
     @Test
     fun testThing() {
-        chatService.respondToUserMessage("1", "Hello")
+        chatService.respondToUserMessage(
+            ConversationSession(chatMemory),
+            "Hello"
+        )
     }
 }
